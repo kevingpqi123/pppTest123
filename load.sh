43f1cf77fb4d3ceff32e28238f1fa00bce296136
kevingpqi123 <kevingpqi@gmail.com>
2022-01-28 14:52:14 +0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [! -e libpag ];then
-    git clone -b main https://github.com/kevingpqi123/libpag.git
-fi
+git branch
+
+git clone -b main https://github.com/kevingpqi123/libpag.git
