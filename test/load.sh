ca527dfcc1bdf3d028fa65b9823039bff4eb4e35
kevingpqi <kevingpqi@tencent.com>
2022-01-31 01:25:22 +0800

    test

---
+++ test/load.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git branch
+
+git clone -b main https://github.com/kevingpqi123/libpag.git
+
+tar -czvf libpag.tar.gz libpag
---
+++ test/load_1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git branch
+
+tar -zxvf libpag.tar.gz
